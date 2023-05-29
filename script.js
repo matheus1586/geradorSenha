520d9b01f848fdf34d3c8ca2874a30cbb3b2286d
Matheus Lira <matheuslira.dev@gmail.com>
2023-05-29 11:21:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let typed = new Typed(".auto-input", {
+  strings: ["Gerador de Senha", "Password Generator"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345!@#$%^&*()";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function () {
+  sizePassword.innerHTML = this.value;
+};
+
+function generatePassword() {
+  let pass = "";
+
+  for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha=pass;
+
+
+
+}
+
+function copyPassword() {
+    navigator.clipboard.writeText(novaSenha);
+    alert("Senha copiada com sucesso!");
+}
